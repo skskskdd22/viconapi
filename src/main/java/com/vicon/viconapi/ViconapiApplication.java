ef3617bf167ef15278e7223bc2d61b63bd4479c9
skskskdd22 <skskskdd22@naver.com>
2022-09-27 11:18:59 +0900

    첫커밋

---
+++ src/main/java/com/vicon/viconapi/ViconapiApplication.java
@@ -0,0 +1,13 @@
+package com.vicon.viconapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViconapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViconapiApplication.class, args);
+    }
+
+}
